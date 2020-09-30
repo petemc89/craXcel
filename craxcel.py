409a40fe18ae8832577285aebd10831866bb322d
Pete McGranaghan <pete.mcgranaghan@outlook.com>
2020-09-30 22:29:34 +0100

    Starts work on v2

--- craxcel.py
+++ craxcel.py
@@ -3,154 +3,121 @@ import errno
 import os
 import shutil
 import sys
+import uuid
 import zipfile
 from lxml import etree
 
-ZIP = '.zip'
-TEMP_DIR = '/temp'
-BACKUP_EXT = ' - backup'
-WORKBOOK_REL = '/xl/workbook.xml'
-WORKSHEETS_REL = '/xl/worksheets/'
-WORKBOOK_TAG = 'workbookProtection'
-WORKSHEET_TAG = 'sheetProtection'
+APP_NAME = 'craXcel'
+USER_HOME_DIR = os.path.expanduser('~')
+APP_BASE_DIR = os.path.join(USER_HOME_DIR, APP_NAME)
+APP_PROCESSING_DIR = os.path.join(APP_BASE_DIR, 'temp')
+
+class FileInfo():
+    def __init__(self, filepath):
+        self.full_name = filepath
+        self.name = os.path.basename(filepath)
+        self.directory, self.extension = os.path.splitext(filepath)    
 
 class ExcelFile():
-    def __init__(self,filepath):
-        self.filepath = filepath
-        self.folderpath = os.path.dirname(filepath)
-        self.extension = os.path.splitext(filepath)[1]
-        self.unpacked_folderpath = self.folderpath + TEMP_DIR
-        self.zipped_filepath = self.filepath.replace(self.extension, ZIP)
-        self.workbook_xml = self.unpacked_folderpath + WORKBOOK_REL
-        self.worksheet_folder = self.unpacked_folderpath + WORKSHEETS_REL
+    def __init__(self, args):
+        self.file = FileInfo(args.filepath)
+        self._args = args
+        self._temp_processing_dir = os.path.join(APP_PROCESSING_DIR, str(uuid.uuid4()))
+        self._xml_root_dir = 'xl'
+        self._workbook_xml_filepath = os.path.join(self._temp_processing_dir, self._xml_root_dir, 'workbook.xml')
+        self._worksheet_xml_dir = os.path.join(self._temp_processing_dir, self._xml_root_dir, 'worksheets')
+        self._workbook_tag_names = ['fileSharing', 'workbookProtection']
+        self._worksheet_tag_names = ['sheetProtection']
 
-    def _get_backup_filepath(self,backup_suffix=BACKUP_EXT):
-        backup_ext = backup_suffix + self.extension
+    def unlock(self):
+        self._unpackage()
 
-        return self.filepath.replace(self.extension,backup_ext)       
+        if self._args.workbook:
+            self._remove_workbook_protection()
+        elif self._args.worksheet:
+            self._remove_worksheet_protection()
+        else:
+            self._remove_workbook_protection()
+            self._remove_worksheet_protection()
+            
+        self._repackage()
 
-    def _change_extension_to_zip(self):
-        os.rename(self.filepath,self.zipped_filepath)
+        if self._args.debug == False:
+            self._cleanup()
 
-    def _delete_zip(self):
-        os.remove(self.zipped_filepath)
-
-    def _change_extension_to_original(self):
-        os.rename(self.zipped_filepath,self.filepath)
-
-    def _get_worksheets(self):
-        return _get_file_listing(self.worksheet_folder)
-
-    def backup(self):
-        shutil.copyfile(self.filepath,self._get_backup_filepath())
-        print('Backup created...')
-
-    def unpackage(self):
-        self._change_extension_to_zip()
-        zipfile.ZipFile(self.zipped_filepath,'r').extractall(self.unpacked_folderpath)
-        self._delete_zip()
+    def _unpackage(self):
+        zipfile.ZipFile(self.file.full_name,'r').extractall(self._temp_processing_dir)
         print('File unpacked...')
 
-    def unprotect_workbook(self):
-        wb = WorkbookXML(self.workbook_xml)
-        wb._remove_protection_element()
-        print('Workbook unprotected...')
+    def _remove_workbook_protection(self):
+        self._remove_protection_element(self._workbook_xml_filepath, self._workbook_tag_names)
+        print('Workbook protection removed...')
 
-    def unprotect_worksheets(self):
-        sheet_count = 0
-        for ws_file in self._get_worksheets():
-            ws = WorksheetXML(ws_file)
-            ws._remove_protection_element()
-            sheet_count += 1
-            (f'Worksheet {sheet_count} unprotected...')
+    def _remove_worksheet_protection(self):
+        worksheet_xml_filepaths = self._get_file_listing(self._worksheet_xml_dir)
 
-    def repackage(self):
-        filepaths = _get_file_listing(self.unpacked_folderpath)
-        with zipfile.ZipFile(self.zipped_filepath,'w') as repackaged_zip:
+        for xml_filepath in worksheet_xml_filepaths:
+            self._remove_protection_element(xml_filepath, self._worksheet_tag_names)
+        print('Worksheet protection removed...')
+
+    def _repackage(self):
+        file_suffix = f'_{APP_NAME}{self.file.extension}'
+        filename = self.file.name.replace(self.file.extension, file_suffix)
+        unlocked_filepath = os.path.join(APP_BASE_DIR, filename)
+
+        filepaths = self._get_file_listing(self._temp_processing_dir)
+        with zipfile.ZipFile(unlocked_filepath,'w') as repackaged_zip:
             for filepath in filepaths:
-                rel_filepath = filepath.replace(self.unpacked_folderpath,'')
+                rel_filepath = filepath.replace(self._temp_processing_dir,'')
                 repackaged_zip.write(filepath,arcname=rel_filepath)
             
-        self._change_extension_to_original()
         print('File repackaged...')
 
-    def cleanup(self):
-        shutil.rmtree(self.unpacked_folderpath)
+    def _cleanup(self):
+        shutil.rmtree(self._temp_processing_dir)
         print('Cleaning up temporary files...')
 
-class WorkbookXML():
-    def __init__(self,filepath):
-        self.filepath = filepath
-        self.protection_tag = WORKBOOK_TAG
-       
-    def _remove_protection_element(self):
-        tree = etree.parse(self.filepath)
+    def _remove_protection_element(self, xml_filepath, tag_names_to_remove):
+        tree = etree.parse(xml_filepath)
         root = tree.getroot()
 
         for element in root.iter():
-            if self.protection_tag in element.tag:
-                root.remove(element)
+            for tag_name in tag_names_to_remove:
+                if tag_name in element.tag:
+                    root.remove(element)
 
-        tree.write(self.filepath, encoding='UTF-8', xml_declaration=True)
+        tree.write(xml_filepath, encoding='UTF-8', xml_declaration=True)
 
-class WorksheetXML():
-    def __init__(self,filepath):
-        self.filepath = filepath
-        self.protection_tag = WORKSHEET_TAG
+    def _get_file_listing(self, directory):
+            filepaths = []
+            for root, folder, files in os.walk(directory): 
+                for filename in files:
+                    filepath = os.path.join(root, filename) 
+                    filepaths.append(filepath)
 
-    def _remove_protection_element(self):
-        tree = etree.parse(self.filepath)
-        root = tree.getroot()
-
-        for element in root.iter():
-            if self.protection_tag in element.tag:
-                root.remove(element)
-
-        tree.write(self.filepath, encoding='UTF-8', xml_declaration=True)
-
-def _get_file_listing(folder):
-        filepaths = []
-        for root, folder, files in os.walk(folder): 
-            for filename in files:
-                filepath = os.path.join(root, filename) 
-                filepaths.append(filepath)
-
-        return filepaths
+            return filepaths
         
 def Main():
     parser = argparse.ArgumentParser(description='Remove Workbook and Worksheet protection on Microsoft Excel files.')
-    parser.add_argument('file', help='Filepath of the Excel file to be unlocked')
+    parser.add_argument('filepath', help='Filepath of the Excel file to be unlocked')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-ws', '--worksheet', action='store_true', 
                         help='unlocks the Worksheets only (leaves Workbook Protection intact)')
     group.add_argument('-wb', '--workbook', action='store_true',
                         help='unlocks the Workbook only (leaves Worksheet Protection intact)')
-    parser.add_argument('--no_backup', action='store_true',
-                        help='runs craXcel without making a backup of the original (use at own risk)')
     parser.add_argument('--debug', action='store_true',
-                        help=f'retains the {TEMP_DIR} folder. Useful for dubugging exceptions')                        
+                        help=f'retains the temp folder. Useful for dubugging exceptions')                        
     args = parser.parse_args()
 
     print('\nStarting craXcel...')
-    print(f'Checking file {args.file}...')
+    print(f'Checking file {args.filepath}...')
 
-    if os.path.isfile(args.file):
-        cxl = ExcelFile(args.file)
+    if os.path.isfile(args.filepath):
+        cxl = ExcelFile(args)
         print('File accepted...')
 
-        if args.no_backup == False:
-            try:
-                cxl.backup()
-            except Exception as e:
-                print(e)
-                print("""
-                Could not create backup, terminating program.
-                You can try running the program with option --no_backup to circumvent this error (use this at your own risk!)
-                """)
-                sys.exit()
-
         try:
-            cxl.unpackage()
+            cxl.unlock()
         except Exception as e:
             print(e)
             print("""
@@ -158,35 +125,6 @@ def Main():
             The Excel format may not be supported. Check the list of supported Excel formats at the github repo.
             """)
             sys.exit()
-
-        if args.worksheet:
-            try:
-                cxl.unprotect_worksheets()
-            except Exception as e:
-                print(e)
-                if args.debug == False:
-                    cxl.cleanup()
-        elif args.workbook:
-            try:
-                cxl.unprotect_workbook()
-            except Exception as e:
-                print(e)
-                if args.debug == False:
-                    cxl.cleanup()
-        else:
-            try:
-                cxl.unprotect_worksheets()
-            except Exception as e:
-                print(e)
-            try:                
-                cxl.unprotect_workbook()
-            except Exception as e:
-                print(e)                      
-
-        cxl.repackage()
-
-        if args.debug == False:
-            cxl.cleanup()
 
     else:
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), args.file)
